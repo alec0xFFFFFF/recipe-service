76a62458229fee5a928f2d80a506d95c413e7887
alec0xFFFFFF <alexander.k.white@gmail.com>
2024-01-23 18:21:48 -0800

    run create extension

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ app = create_api()
 
 with app.app_context():
     db.create_all()
+    try:
+        db.session.execute("CREATE EXTENSION IF NOT EXISTS pg_similarity;")
+        db.session.commit()
+        print("pg_similarity extension created successfully.")
+    except Exception as e:
+        db.session.rollback()
+        print(f"Error creating pg_similarity extension: {str(e)}")
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getos.environ.get("PORT", default=5000))
