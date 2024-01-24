e6acb7848234cebf585440f3a1082a83a42cda18
alec0xFFFFFF <alexander.k.white@gmail.com>
2024-01-24 08:20:30 -0800

    fix query

--- api/v1/routes.py
+++ api/v1/routes.py
@@ -77,7 +77,7 @@ def search_for_recipe():
 
     embeddings = agent.get_embedding(query_string)
     sql_query = text("""
-        SELECT * FROM
+        SELECT * FROM recipes
         WHERE id IN (
         SELECT recipe_id FROM description_embeddings
         ORDER BY embeddings <-> CAST(:embeddings AS vector)
