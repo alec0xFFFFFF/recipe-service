0ba98b9c96dd6c13ebd839a5092250d7fca0231e
alec0xFFFFFF <alexander.k.white@gmail.com>
2024-01-23 16:46:42 -0800

    use port var

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
+
 from app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(debug=True, port=os.getos.environ.get("PORT", default=5000))
